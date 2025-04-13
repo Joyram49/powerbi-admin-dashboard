bd5d23e736c3fe680428947ca44bb9aeb5e929b8
Yaseer123 <yaseerarafat230@gmail.com>
2025-04-13 15:17:42 +0600

    billing related schema added

---
+++ packages/db/src/schema/billing.ts
@@ -0,0 +1,31 @@
+import {
+  numeric,
+  pgTable,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+import { companies } from "./company";
+
+export const billings = pgTable("billing", {
+  id: uuid("id").notNull().primaryKey().defaultRandom(),
+  companyId: uuid("company_id").references(() => companies.id, {
+    onDelete: "set null",
+  }),
+  stripeCustomerId: varchar("stripe_customer_id", { length: 255 }),
+  billingDate: timestamp("billing_date", { withTimezone: true }).notNull(),
+  status: varchar("status", { length: 255 }).notNull(),
+  amount: numeric("amount", { precision: 10, scale: 2 }).notNull(),
+  plan: varchar("plan", { length: 255 }).notNull(),
+  pdfLink: varchar("pdf_link", { length: 255 }),
+  paymentStatus: varchar("payment_status", { length: 255 }).notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+  dateCreated: timestamp("date_created", { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+});
+
+export type Billing = typeof billings.$inferSelect;
